0a726070a6ee5aab86f32b9c9a3c9fe8df424aff
Willian Parente <willianparente@hotmail.com>
2019-07-11 21:41:14 -0300

    adiçao dos graficos

---
+++ assets/js/charts.js
@@ -0,0 +1,39 @@
+( function ( $ ) {
+    "use strict";
+    
+    //bar chart
+    var ctx = document.getElementById( "barChart" );
+    //    ctx.height = 200;
+    var myChart = new Chart( ctx, {
+        type: 'bar',
+        data: {
+            labels: [ "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set" ],
+            datasets: [
+                {
+                    label: "Atual",
+                    data: [ 95, 10, 75, 55, 87, 53, 75, 70, 68 ],
+                    //borderColor: "rgba(0, 194, 146, 0.9)",
+                    borderWidth: "0",
+                    backgroundColor: "rgba(0, 194, 146, 0.5)"
+                            },
+                {
+                    label: "Período anterior",
+                    data: [ 60, 72, 62, 68, 63, 49, 72, 55, 51 ],
+                    //borderColor: "rgba(0,0,0,0.09)",
+                    borderWidth: "0",
+                    backgroundColor: "rgba(0,0,0,0.07)"
+                            }
+                        ]
+        },
+        options: {
+            scales: {
+                yAxes: [ {
+                    ticks: {
+                        beginAtZero: false
+                    }
+                                } ]
+            }
+        }
+    } );
+
+} )( jQuery );
---
+++ assets/js/flot-charts.js
@@ -0,0 +1,61 @@
+(function($){
+
+    "use strict"; // Start of use strict
+   
+    var SufeeAdmin = {
+   
+       line2Flot: function(){
+   
+           // first chart
+           var chart1Options = {
+               series: {
+                   lines: {
+                       show: true
+                   },
+                   points: {
+                       show: true
+                   }
+               },
+               xaxis: {
+                   mode: "time",
+                   timeformat: "%m/%d",
+                   minTickSize: [ 1, "day" ]
+               },
+               grid: {
+                   hoverable: true
+               },
+               legend: {
+                   show: false
+               },
+               grid: {
+                   color: "#fff",
+                   hoverable: true,
+                   borderWidth: 0,
+                   backgroundColor: 'transparent'
+               },
+               tooltip: {
+                   show: true,
+                   content: "y: %y"
+               }
+           };
+           var chart1Data = {
+               label: "chart1",
+               color: "#007BFF",
+               data: [
+             [ 1354521600000, 6322 ],
+             [ 1354840000000, 6340 ],
+             [ 1355223600000, 6368 ],
+             [ 1355306400000, 6374 ],
+             [ 1355487300000, 6388 ],
+             [ 1355571900000, 6400 ]
+           ]
+           };
+           $.plot( $( "#chart1" ), [ chart1Data ], chart1Options );
+       }
+   
+   };
+   
+   $(document).ready(function() {
+       SufeeAdmin.line2Flot();});
+   
+   })(jQuery);
